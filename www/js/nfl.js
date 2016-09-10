00a88d54eef118a02411f092a561166ef476f92e
Patrick L Archibald <patrick.archibald@gmail.com>
2016-09-10 15:16:54 -0400

    Add image refresh if current week.

--- www/js/nfl.js
+++ www/js/nfl.js
@@ -23,19 +23,24 @@ $(function() {
 });
 $.getJSON("/TextFeed2Images/JsonServlet?action=weeks", function(data) {
   weeks = data;
-  console.log(weeks.length + " weeks Index: " + index);
-  for (index = 0 ; index < weeks.length && !weeks[index].current; index++) {
-    console.log(weeks[index].current);
-  }
+  console.log(weeks.length + " weeks.");
+  for (index = 0; index < weeks.length && !weeks[index].current; index++);
   console.log("index: " + index);
   $('.toast').fadeIn(400).delay(3000).fadeOut(400);
 });
 
+function refreshIfCurrentWeek() {
+  if (weeks[index].current) {
+    $("#image").attr("src", weeks[index].imageUrl + "?epoch=" + (new Date).getTime());
+  }
+}
 
 function set_body_height() { // set body height = window height
-   $('body').height($(window).height());
- }
- $(document).ready(function() {
-   $(window).bind('resize', set_body_height);
-   set_body_height();
- });
+  $('body').height($(window).height());
+}
+
+$(document).ready(function() {
+  $(window).bind('resize', set_body_height);
+  set_body_height();
+  setInterval(refreshIfCurrentWeek, 60000);
+});
