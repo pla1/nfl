302ecd684bc8bc7a1eab1a13c56db8d8e31d7157
Patrick L Archibald <patrick.archibald@gmail.com>
2016-09-10 21:20:11 -0400

    More works on teams.

--- www/js/teams.js
+++ www/js/teams.js
@@ -2,12 +2,23 @@ var app = angular.module('app', []);
 
 app.controller('Controller', function appControllerFunction($scope, $http) {
   $scope.teams = [];
+  $scope.years = [];
+  $scope.settings = {};
+  $scope.settings.year = new Date().getFullYear();
+  $scope.settings.teamId = "CAR";
+  $scope.settings.imageUrl = "NFL256x256.png";
   $http.get('/TextFeed2Images/JsonServlet?action=teams').success(
       function(data) {
         $scope.teams = data;
       }
     );
-    $scope.team = function(team) {
-      console.log(JSON.stringify(team));
+    $http.get('/TextFeed2Images/JsonServlet?action=years').success(
+      function(data) {
+        $scope.years = data;
+      }
+    );
+    $scope.change = function() {
+      console.log("Year: "  + $scope.settings.year + " teamId: " + $scope.settings.teamId);
+      $scope.settings.imageUrl="/TextFeed2Images/images/"+$scope.settings.year+$scope.settings.teamId+".png";
     }
 });
